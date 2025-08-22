320189e6f0d1854e13b2dc69538d3c3da18d6f44
Suzy-Lana777 <reutasvetlana382@gmail.com>
2025-08-22 21:06:30 +0300

    fix deploy versel

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -1,29 +1,5 @@
 // app/@modal/(.)notes/[id]/page.tsx
 
-// import { getSingleNote } from '@/lib/api';
-// import Modal from '@/components/Modal/Modal';
-// import { useRouter } from 'next/navigation';
-// import React from 'react';
-
-// type Props = {
-//   params: { id: string };
-// };
-
-// export default async function NotePreview({ params }: Props) {
-//   const { id } = params;
-//   const note = await getSingleNote(id);
-
-//   const router = useRouter();
-//   const handleClose = () => router.back();
-
-//   return (
-//     <Modal onClose={handleClose}>
-//       <h2>{note.title}</h2>
-//       <p>{note.content}</p>
-//     </Modal>
-//   );
-// }
-
 import { getSingleNote } from "@/lib/api";
 import { QueryClient, dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import NotePreviewClient from "./NotePreview.client";
