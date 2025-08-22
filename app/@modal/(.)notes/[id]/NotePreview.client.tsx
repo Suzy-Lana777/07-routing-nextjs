2dc6f65ae4fc81da042067ca8a7c63ff1b4062e2
Suzy-Lana777 <reutasvetlana382@gmail.com>
2025-08-22 21:58:31 +0300

    fix problem with versel

--- app/@modal/(.)notes/[id]/NotePreview.client.tsx
+++ app/@modal/(.)notes/[id]/NotePreview.client.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import css from "./NotePreview.module.css"
+import css from "./NotePreview.client.module.css"
 import { getSingleNote } from '@/lib/api';
 import { useQuery } from "@tanstack/react-query"
 import { useRouter } from "next/navigation";
