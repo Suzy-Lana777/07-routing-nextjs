2045ea669bfaf2b5c4e3629a4e561b90a78aec95
Suzy-Lana777 <reutasvetlana382@gmail.com>
2025-08-23 07:58:45 +0300

    versel

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -1,28 +1,54 @@
 // app/@modal/(.)notes/[id]/page.tsx
 
+// import { getSingleNote } from "@/lib/api";
+// import { QueryClient, dehydrate, HydrationBoundary } from "@tanstack/react-query";
+// import NotePreviewClient from "./NotePreview.client";
+
+// interface NotePreviewProps {
+//     params: Promise<{ id: string }>;
+// };
+
+// const NotePreview = async ({ params }: NotePreviewProps) => {
+//     const { id } = await params;
+
+//     const queryClient = new QueryClient();
+
+//     await queryClient.prefetchQuery({
+//         queryKey: ["note", id],
+//         queryFn: () => getSingleNote(id),
+//     })
+
+//     return (
+//         <HydrationBoundary state={dehydrate(queryClient)}>
+//             <NotePreviewClient id={id} />
+//         </HydrationBoundary>
+//     );
+// };
+
+// export default NotePreview;
+
 import { getSingleNote } from "@/lib/api";
 import { QueryClient, dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import NotePreviewClient from "./NotePreview.client";
 
 interface NotePreviewProps {
-    params: Promise<{ id: string }>;
-};
+  params: { id: string };
+}
 
 const NotePreview = async ({ params }: NotePreviewProps) => {
-    const { id } = await params;
+  const { id } = params; 
+  const queryClient = new QueryClient();
 
-    const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["note", id],
+    queryFn: () => getSingleNote(id),
+  });
 
-    await queryClient.prefetchQuery({
-        queryKey: ["note", id],
-        queryFn: () => getSingleNote(id),
-    })
-
-    return (
-        <HydrationBoundary state={dehydrate(queryClient)}>
-            <NotePreviewClient id={id} />
-        </HydrationBoundary>
-    );
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NotePreviewClient id={id} />
+    </HydrationBoundary>
+  );
 };
 
 export default NotePreview;
